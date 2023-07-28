81c4c3daa8bf51924731be17e2e73b352a7d009c
dilhansiriwardhana <76891526+dilhansiriwardhana@users.noreply.github.com>
2023-07-28 16:01:45 +0530

    adding list to dom

---
+++ 00-start/shopping-list/script.js
@@ -0,0 +1,25 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+    e.preventDefault();
+
+
+    // Validate Input
+    if (itemInput.value === '') {
+        alert('Please add an item');
+        return;
+    }
+
+    console.log('Success');
+
+}
+
+
+//  Event Listeners
+itemForm.addEventListener('submit' , addItem);
+
+
+
+
