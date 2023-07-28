a80ab4857a4847e07ee599ee51367623ad57599e
dilhansiriwardhana <76891526+dilhansiriwardhana@users.noreply.github.com>
2023-07-28 22:46:51 +0530

    Update script.js

--- 00-start/shopping-list/script.js
+++ 00-start/shopping-list/script.js
@@ -5,16 +5,31 @@ const itemList = document.getElementById('item-list');
 function addItem(e) {
     e.preventDefault();
 
+    const newItem = itemInput.value;
 
     // Validate Input
-    if (itemInput.value === '') {
+    if (newItem === '') {
         alert('Please add an item');
         return;
     }
 
-    console.log('Success');
+    //  Create List
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    const button = createButton ('remove-item btn-link text-red')
+    console.log(button);
+
 
 }
+
+function createButton (classes) {
+    const button = document.createElement('button');
+    button.className = classes;
+    return button;
+
+}
+
 
 
 //  Event Listeners
