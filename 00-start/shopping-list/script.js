833e0a0c387afa317142d47a2ede3c12a3cc0afd
dilhansiriwardhana <76891526+dilhansiriwardhana@users.noreply.github.com>
2023-07-29 12:19:06 +0530

    clear button defined

--- 00-start/shopping-list/script.js
+++ 00-start/shopping-list/script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const ClearBtn = document.getElementById('clear');
+
 
 function addItem(e) {
     e.preventDefault();
@@ -20,8 +22,9 @@ function addItem(e) {
     const button = createButton ('remove-item btn-link text-red')
     li.appendChild(button);
 
-    console.log(li);
-   
+    itemList.appendChild(li);
+
+    itemInput.value = '';
 
 
 }
@@ -42,9 +45,27 @@ function createIcon(classes) {
 }
 
 
+function removeItem (e) {
+    if(e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove();
+    }
+    
+
+}
+
+function clearItems() {
+    while(itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+        
+    }
+}
+
+
 
 //  Event Listeners
 itemForm.addEventListener('submit' , addItem);
+itemList.addEventListener('click', removeItem);
+ClearBtn.addEventListener('click', clearItems);
 
 
 
